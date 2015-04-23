2b75e26541cccaf0483b453ede4f4477cd40a442
Joseph Bieselin <joebieselin@gmail.com>
2015-04-23 03:34:33 -0400

    check superblock data done

--- csefsck.py
+++ csefsck.py
@@ -3,24 +3,25 @@
 
 # CONSTANTS
 
-MAX_NUM_BLOCKS	= 10000
-MAX_FILE_SIZE	= 1638400
-BLOCK_SIZE		= 4096
-DEV_ID			= 20
-FREE_START		= 1
-FREE_END		= 25
-ROOT			= FREE_END + 1
-BLOCKS_IN_FREE	= 400
-UID				= 1
-GID				= 1
-DIR_UID			= 1000
-DIR_GID			= 1000
+MAX_NUM_BLOCKS   = 10000
+MAX_FILE_SIZE    = 1638400
+BLOCK_SIZE       = 4096
+DEV_ID		 = 20
+FREE_START	 = 1
+FREE_END	 = 25
+ROOT	         = FREE_END + 1
+BLOCKS_IN_FREE	 = 400
+UID		 = 1
+GID		 = 1
+DIR_UID		 = 1000
+DIR_GID		 = 1000
 
-FILES_DIR		= "/fusedata"
-LOG_FILE		= "~/Desktop/log.txt"
+FILES_DIR	 = "/fusedata"
+LOG_FILE	 = "~/Desktop/log.txt"
 
 
 from time import time
+from sys import exit
 
 
 
@@ -30,6 +31,7 @@ def get_file_content(filepath):
     the_file.close()
     return content
 
+# ---------------------------------- 1 ----------------------------------------#
 # Returns a boolean based on the check if the device ID that is stored in the superblock, aka fusedata.0, is the correct ID
 def check_devId():
     # read the contents of the superblock file into the variable: contents
@@ -47,9 +49,8 @@ def check_devId():
     test_devId_num = int(test_devId_str) # convert it to a variable of type int
 
     if (DEV_ID != test_devId_num):
-        file_list[2] = file_list[2].replace(test_devId_str, str(DEV_ID))
-        contents = ','.join(file_list)
-        # assume for simplicity that file size of superblock does not pass BLOCK_SIZE
+        print "Device ID did not match the expected value... awkward\n"
+        exit(1)
     
     # pad the file's contents with 0's
     contents = contents.ljust(BLOCK_SIZE, '0')
@@ -57,16 +58,12 @@ def check_devId():
     superblock.seek(0)
     superblock.write(contents)
     superblock.close()
+# ---------------------------------- 1 ----------------------------------------#
 
-# Returns a boolean based on the check if the passed in time 't' is greater than the time denoted in the superblock, aka fusedata.0,
-def check_superblock_time(t):
-    # read the contents of the superblock file into the variable: contents
-    superblock_path = FILES_DIR + "/fusedata.0"
-    superblock = open(superblock_path, 'r+')
-    contents = superblock.read()
-    contents = contents.strip() # strip any whitespace
-    # break contents into a list separated by commas
-    file_list = contents.split(',')
+# ---------------------------------- 2 ----------------------------------------#
+
+# check and possibly update the superblock's creationTime
+def check_superblock_time(t, file_list):
     # break the creationTime part into a 2 component list
     test_ctime_list = file_list[0].split(':')
     # put the stringified creationTime number into test_ctime
@@ -75,34 +72,172 @@ def check_superblock_time(t):
     test_ctime_num = int(test_ctime_str) # convert it to a variable of type int
     
     if (t < test_ctime_num):
+        print "Time in the superblock was a future value and is now the current time\n"
         file_list[0] = file_list[0].replace(test_ctime_str, str(t))
-        contents = ','.join(file_list)
         # assme for simplicity that file size of superblock does not pass BLOCK_SIZE
+
+# check and possibly update the superblock's fusedata block info
+def check_superblock_block_data(t, file_list):
+    # break the block data info part into 4 2 component lists
+    test_data_list = []
+    # append freeStart, freeEnd, root, and maxBlocks data to the list, in that indexed order
+    for i in range(3, 7):
+        test_data_list.append(file_list[i].split(':'))
+    # for each data value, test for its correctness in reference to global consts
+    for j in range(0, 4):
+        # put the stringified int value to test into test_data
+        test_data = test_data_list[j][1]
+        # strip any whitespace, and if it is index 4 (maxBlocks), strip the ending '}' char
+        test_data_str = test_data.strip()
+        if (j == 3):
+            test_data_str = test_data_str.rstrip('0')
+            test_data_str = test_data_str.rstrip('}')
+        test_data_num = int(test_data_str) # convert it to a variable of type int
+        
+        # determine which case is being tested and update the appropriate value if needed
+        if (j == 0): # freeStart
+            if (test_data_num != FREE_START):
+                print "freeStart in the superblock was incorrect and has been corrected\n"
+                file_list[3] = file_list[3].replace(test_data_str, str(FREE_START))
+        elif (j == 1): # freeEnd
+            if (test_data_num != FREE_END):
+                print "freeEnd in the superblock was incorrect and has been corrected\n"                
+                file_list[4] = file_list[4].replace(test_data_str, str(FREE_END))
+        elif (j == 2): # root
+            if (test_data_num != ROOT):
+                print "root in the superblock was incorrect and has been corrected\n"
+                file_list[5] = file_list[5].replace(test_data_str, str(ROOT))
+        else: # maxBlocks
+            if (test_data_num != MAX_NUM_BLOCKS):
+                print "maxBlocks in the superblock was incorrect and has been corrected\n"
+                file_list[6] = file_list[6].replace(test_data_str, str(MAX_NUM_BLOCKS))
+    
+# checks and updates (if needed) the superblock's creationTime, freeStart, freeEnd, root, and maxBlocks entries
+def check_superblock(t):
+    # read the contents of the superblock file into the variable: contents
+    superblock_path = FILES_DIR + "/fusedata.0"
+    superblock = open(superblock_path, 'r+')
+    contents = superblock.read()
+    contents = contents.strip() # strip any whitespace
+    # break contents into a list separated by commas
+    file_list = contents.split(',')
+    
+    # check the superblock's data (file_list is a container and can be used by reference in functions)
+    check_superblock_time(t, file_list)
+    check_superblock_block_data(t, file_list)
+    # get the superblock content back into string format
+    contents = ','.join(file_list)
     
     # pad the file's contents with 0's
     contents = contents.ljust(BLOCK_SIZE, '0')
     superblock.seek(0)
     superblock.write(contents)
+    superblock.truncate(BLOCK_SIZE) # truncate the file, assuming data inside will not exceed 4096 bytes
     superblock.close()
-    
+
 '''
 Checks the atime, ctime, mtime for the current entry in the file system, and if any are greater than the passed in time 't', return False.
 Recursively checks all entries in filename_to_inode_dict if this entry is a directory.
 The recursive calls will return booleans based on each respective entry's atime, ctime, mtime comparison with time 't'.
 entry_type will either be 'd' or 'f' corresponding to 'directory' and 'file inode', respectively.
 '''
-def check_entry_times(t, entry_type):
+def check_entry_times(t, entry_type, num):
+    # read the contents of the fusedata block into the variable: contents
+    block_path = FILES_DIR + "/fusedata." + str(num)
+    block = open(block_path, 'r+')
+    contents = block.read()
+    contents = contents.strip() # strip any whitespace
+    # break contents into a list separated by commas
+    file_list = contents.split(',')
     
-    return True
+    if (entry_type == 'f'):
+        file_list_atime_index = 5
+        file_list_ctime_index = 6
+        file_list_mtime_index = 7
+    else:
+        file_list_atime_index = 4
+        file_list_ctime_index = 5
+        file_list_mtime_index = 6
+
+    # break the times into 2 component lists
+    test_atime_list = file_list[file_list_atime_index].split(':')
+    test_ctime_list = file_list[file_list_ctime_index].split(':')
+    test_mtime_list = file_list[file_list_mtime_index].split(':')
+    
+    # atime
+    test_atime = test_atime_list[1]
+    test_atime_str = test_atime.strip() # strip any whitespace from atime number
+    test_atime_num = int(test_atime_str) # convert it to a variable of type int
+    # ctime
+    test_ctime = test_ctime_list[1]
+    test_ctime_str = test_ctime.strip() # strip any whitespace from ctime number
+    test_ctime_num = int(test_ctime_str) # convert it to a variable of type int
+    # mtime
+    test_mtime = test_mtime_list[1]
+    test_mtime_str = test_mtime.strip() # strip any whitespace from mtime number
+    test_mtime_num = int(test_mtime_str) # convert it to a variable of type int
+    
+    changed = False
+    # update atime if it is a value in the future
+    if (t < test_atime_num):
+        changed = True
+        print "atime in fusedata.%d was a future value and is now the current time\n" % num
+        file_list[file_list_atime_index] = file_list[file_list_atime_index].replace(test_atime_str, str(t))
+        # contents = ','.join(file_list)
+        # assme for simplicity that file size of superblock does not pass BLOCK_SIZE
+
+    # update ctime if it is a value in the future                
+    if (t < test_ctime_num):
+        changed = True
+        print "ctime in fusedata.%d was a future value and is now the current time\n" % num
+        file_list[file_list_ctime_index] = file_list[file_list_atime_index].replace(test_ctime_str, str(t))
+        # contents = ','.join(file_list)
+        # assme for simplicity that file size of superblock does not pass BLOCK_SIZE
+
+    # update mtime if it is a value in the future
+    if (t < test_mtime_num):
+        changed = True
+        print "mtime in fusedata.%d was a future value and is now the current time\n" % num
+        file_list[file_list_mtime_index] = file_list[file_list_mtime_index].replace(test_mtime_str, str(t))
+        # contents = ','.join(file_list)
+        # assme for simplicity that file size of superblock does not pass BLOCK_SIZE
+    
+    # if we changed any of the time values, update the contents variable
+    if (changed):
+        contents = ','.join(file_list)
+    
+    # pad the file's contents with 0's
+    contents = contents.ljust(BLOCK_SIZE, '0')
+    block.seek(0)
+    block.write(contents)
+    block.close()
+    
+    # if this fusedata block is a directory, recursively call this function on all directory entries (except '.' and '..')   
+    if (entry_type == 'd'):
+        # split the file contents by '{' to separate some of the filename to inode dict
+        file_list = contents.split('{')
+        # split again by '}' to get the inode_list as the first index of the created list
+        inode_list = file_list[2].split('}')
+        # now the inode_list is actually a list with each directory entry
+        inode_list = inode_list[0].split(',')
+        for entry in inode_list:
+            entry = entry.strip()
+            # inode_content is a list: 0 --> type; 1 --> name; 2 --> block number
+            inode_content = entry.split(':')
+            if (inode_content[1] != '.' and inode_content[1] != '..'):
+                # recursive call to check the directory's next entry's time data
+                check_entry_times(t, inode_content[0], inode_content[2])
+        
 
 def check_times():
     time_since_epoch = int(time())
-    
-    return True
-    
+    check_superblock(time_since_epoch)
+    #check_entry_times(time_since_epoch, 'd', ROOT)
+# ---------------------------------- 2 ----------------------------------------#
+
 def main():
-    check_superblock_time(int(time()))
     check_devId()
+    check_times()
     
 if __name__ == "__main__":
-    main()    
+    main()
---
+++ test.py
@@ -0,0 +1,19 @@
+def change_list(listy):
+    j = 0
+    for j in range(len(listy)):
+        listy[j] += "%d" % j
+    '''    
+    for i in listy:
+        print i + '\n'
+        i = '0'
+        j += 1
+    '''
+    
+def main():
+    listy = ['I', 'am', 'joe']
+    change_list(listy)
+    for i in listy:
+        print i + '\n'
+        
+if __name__ == "__main__":
+    main()
